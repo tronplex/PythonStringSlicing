0ac2fa3db72e4d65ae05f270086da7499f57fdd4
tronplex <105672844+tronplex@users.noreply.github.com>
2022-05-17 15:16:00 -0500

    Create stringslicing.py

---
+++ stringslicing.py
@@ -0,0 +1,17 @@
+string = "Welcome to Python strings!"
+print(string)
+
+# GET THE WHOLE STRING FROM INDEX 4
+print(string[4:])
+
+# GET THE STRING FROM START UP TO INDEX 4
+print(string[:4])
+
+# PRINT THE ENTIRE STRING WITH SLICING
+print(string[::])
+
+# PRINT CHARACTERS WITH A STEP OF 2
+print(string[::2])
+
+# PRINT THE REVERSE OF THE STRING
+print(string[::-1])
